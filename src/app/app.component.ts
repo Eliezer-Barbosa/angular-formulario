556e23daf3c76124da6e9c33fa83a322d564d18b
Eliezer <eliezer1789j@gmail.com>
2020-02-04 18:02:12 +0000

    Template-driven Forms: Criando um formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-formulario';
+
+  salvar(form: NgForm) {
+    console.log(form);
+  }
+
 }
