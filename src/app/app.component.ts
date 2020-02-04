d6ad1cc8c1d120077e51a8d3017016a6053ede57
Eliezer <eliezer1789j@gmail.com>
2020-02-04 18:42:16 +0000

    Adicionando validação em formulários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,8 @@ export class AppComponent {
     // this.cliente.email = form.value.emailAddress;
     // this.cliente.profissao = form.value.profissao;
 
-    console.log(form.value);
-    console.log(this.cliente);
+    console.log(form);
+    // console.log(this.cliente);
   }
 
 }
